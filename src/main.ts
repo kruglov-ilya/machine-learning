0165005b9a3660d6d6e5612ac90c0ad21fbfd6ce
kruglov-ilya <kruglov-il1@yandex.ru>
2023-10-31 23:30:43 +0400

    feta: complete lab about posnet

--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,112 @@
+import * as tf from "@tensorflow/tfjs-core";
+import * as posenet from "@tensorflow-models/posenet";
 import "@tensorflow/tfjs-backend-webgl";
 
-import * as tf from "@tensorflow/tfjs-core";
-import * as tfData from "@tensorflow/tfjs-data";
-import * as tfLayers from "@tensorflow/tfjs-layers";
+const videoElement: HTMLVideoElement = document.getElementById(
+  "video"
+) as HTMLVideoElement;
+const canvas: HTMLCanvasElement = document.getElementById(
+  "output"
+) as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
-const trainingUrl = "/wdbc-train.csv";
-const testingUrl = "/wdbc-test.csv";
+async function setupCamera(): Promise<void> {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error(
+      "Browser API navigator.mediaDevices.getUserMedia not available"
+    );
+  }
 
-interface DataType extends tf.TensorContainerObject {
-  xs: tf.Tensor;
-  ys: tf.Tensor;
+  videoElement.width = 600;
+  videoElement.height = 500;
+
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  videoElement.srcObject = stream;
+
+  return new Promise((resolve) => {
+    videoElement.onloadedmetadata = () => {
+      canvas.width = videoElement.width;
+      canvas.height = videoElement.height;
+      resolve();
+    };
+  });
 }
 
-type NormalCSVDataset = tfData.Dataset<DataType> & tfData.CSVDataset;
+async function loadPoseNet(): Promise<posenet.PoseNet> {
+  const net = await posenet.load();
+  return net;
+}
 
-async function run() {
-  const trainingData = tfData.csv(trainingUrl, {
-    columnConfigs: {
-      diagnosis: {
-        isLabel: true,
-      },
-    },
-  }) as any as NormalCSVDataset;
+function drawKeypoints(
+  keypoints: posenet.Keypoint[],
+  minConfidence: number,
+  context: CanvasRenderingContext2D
+) {
+  for (const keypoint of keypoints) {
+    if (keypoint.score >= minConfidence) {
+      const { y, x } = keypoint.position;
+      context.beginPath();
+      context.arc(x, y, 10, 0, 2 * Math.PI);
+      context.fillStyle = "aqua";
+      context.fill();
+    }
+  }
+}
 
-  console.log(await trainingData.toArray());
-
-  const convertedTrainingData = trainingData
-    .map(({ xs, ys }) => {
-      return { xs: Object.values(xs), ys: Object.values(ys) };
-    })
-    .batch(10);
-
-  const testingData = tfData.csv(testingUrl, {
-    columnConfigs: {
-      diagnosis: {
-        isLabel: true,
-      },
-    },
-  }) as any as NormalCSVDataset;
-
-  console.log(await testingData.toArray());
-
-  const convertedTestingData = testingData
-    .map(({ xs, ys }) => {
-      return { xs: Object.values(xs), ys: Object.values(ys) };
-    })
-    .batch(10);
-
-  const numOfFeatures = (await trainingData.columnNames()).length - 1;
-
-  // Define the model.
-  const model = tfLayers.sequential();
-  model.add(
-    tfLayers.layers.dense({
-      inputShape: [numOfFeatures],
-      units: 1,
-    })
+function drawSkeleton(
+  keypoints: posenet.Keypoint[],
+  minConfidence: number,
+  context: CanvasRenderingContext2D
+) {
+  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+    keypoints,
+    minConfidence
   );
-  model.compile({
-    optimizer: tf.train.adam(0.06),
-    loss: "categoricalCrossentropy",
-  });
 
-  await model.fitDataset(convertedTrainingData, {
-    epochs: 10,
-    validationData: convertedTestingData,
-    callbacks: {
-      onEpochEnd: async (epoch, logs) => {
-        console.log(
-          "Epoch: " + epoch + " Loss: " + logs?.loss
-        );
-      },
-    },
+  adjacentKeyPoints.forEach((keypoints) => {
+    context.beginPath();
+    context.moveTo(keypoints[0].position.x, keypoints[0].position.y);
+    context.lineTo(keypoints[1].position.x, keypoints[1].position.y);
+    context.lineWidth = 2;
+    context.strokeStyle = "aqua";
+    context.stroke();
   });
-  await model.save("downloads://my_model");
 }
 
-run().then(() => console.log("Done"));
+async function bindPage() {
+  await tf.setBackend("webgl"); // Set the backend to WebGL
+  await tf.ready(); // Wait for the backend to be ready
+
+  await setupCamera();
+  videoElement.play();
+
+  const net = await loadPoseNet();
+  console.log("PoseNet model loaded.");
+
+  detectPoseInRealTime(videoElement, net);
+}
+
+function detectPoseInRealTime(video: HTMLVideoElement, net: posenet.PoseNet) {
+  async function poseDetectionFrame() {
+    const poses = await net.estimatePoses(video, {
+      flipHorizontal: false,
+      decodingMethod: "single-person",
+    });
+
+    ctx.clearRect(0, 0, video.width, video.height);
+    ctx.save();
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+    ctx.restore();
+
+    poses.forEach(({ keypoints }) => {
+      drawKeypoints(keypoints, 0.5, ctx);
+      drawSkeleton(keypoints, 0.5, ctx);
+    });
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}
+
+window.onload = bindPage;
